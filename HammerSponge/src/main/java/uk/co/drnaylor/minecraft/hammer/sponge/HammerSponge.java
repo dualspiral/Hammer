62d61e81a50de30943a82ad41bd991cb49146c77
Daniel Naylor <git@drnaylor.co.uk>
2015-10-14 21:22:53 +0100

    Bump everything to 0.3

--- HammerSponge/src/main/java/uk/co/drnaylor/minecraft/hammer/sponge/HammerSponge.java
+++ HammerSponge/src/main/java/uk/co/drnaylor/minecraft/hammer/sponge/HammerSponge.java
@@ -46,7 +46,7 @@ import java.util.concurrent.TimeUnit;
 @Plugin(id = "hammer", name = "Hammer for Sponge", version = HammerSponge.VERSION)
 public class HammerSponge {
 
-    public static final String VERSION = "0.2";
+    public static final String VERSION = "0.3";
 
     @Inject private Game game;
     @Inject private Logger logger;
